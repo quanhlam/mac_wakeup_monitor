002dbc6aa0a48459423b7e21f0a906fd949eb2b1
quanhlam <quan_lam@mckinsey.com>
2023-12-20 21:51:38 +0700

    support adding variation

--- analytics.py
+++ analytics.py
@@ -1,17 +1,17 @@
+import sys
 import math
 from datetime import datetime, timedelta
 
-
-def is_considered_continuous(seconds, variations = 2):
+def is_considered_continuous(seconds, variations=2):
   return seconds >= 60 - variations  and seconds <= 60 + variations
 
-def get_continuous_segments(lst_times):
+def get_continuous_segments(lst_times, variations=2):
   segments = []
   _cur_cont_segment = []
   for tr in lst_times:
       if not _cur_cont_segment:
         _cur_cont_segment.append(tr)
-      elif is_considered_continuous((tr - _cur_cont_segment[-1]).total_seconds()):
+      elif is_considered_continuous((tr - _cur_cont_segment[-1]).total_seconds(), variations):
         _cur_cont_segment.append(tr)
       else:
         segments.append(_cur_cont_segment)
@@ -27,28 +27,38 @@ def to_human_readable(lst_segments):
   for segment in lst_segments:
       _start = segment[0].strftime("%d/%m %H:%M:%S")
       _end = segment[-1].strftime("%d/%m  %H:%M:%S")
+      secs_diff = (segment[-1] - segment[0]).total_seconds()
+
+      txt = ""
+      desc = ""
+      if secs_diff < 60 * 60:
+        desc = f"{round(secs_diff/(60))} minutes"
+      else:
+        desc = f"{round(secs_diff/(60*60))} hours"
       if len(segment) > 1:
         if _start.split()[0] == _end.split()[0]:
-          desc = ""
-          secs_diff = (segment[-1] - segment[0]).total_seconds()
-          if secs_diff < 60:
-            desc = f"{secs_diff} seconds"
-          elif secs_diff < 60 * 60:
-            desc = f"{math.ceil(secs_diff/(60))} minutes"
-          else:
-            desc = f"{math.ceil(secs_diff/(60*60))} hours"
-          lst_text.append(f"{_start} -> {_end.split()[1]} ({desc})")
+          txt = f"{_start} -> {_end.split()[1]}"
         else:
-          lst_text.append(f"{_start} -> {_end}")
+          txt = f"{_start} -> {_end}"
       else:
-        lst_text.append(_start)
+        txt = _start
+
+      if secs_diff >= 120:
+        txt += f" ({desc})"
+
+      lst_text.append(txt)
   return lst_text
+
+def get_variation_input():
+  if len(sys.argv) > 1:
+      return int(sys.argv[1])
+  return None
 
 
 if __name__ == '__main__':
 
   # Input
-  file = "time-track.log"
+  file = "/Users/Quan_Lam/repos/personal/automations/time-track.log"
   #start_time = "2023-12-16 00:00:00"
   #end_time = "2023-12-18 00:00:00"
   #_start = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
@@ -60,10 +70,11 @@ if __name__ == '__main__':
   f = open(file, "r+")
 
   try:
+    variation = get_variation_input()
     lines = f.readlines()
     fmt_lines = map(lambda x: datetime.strptime(x.strip(), "%d/%m/%Y %H:%M:%S"), lines) 
     time_range = filter(lambda x: x >= _start and x < _end, fmt_lines)
-    continuous_segments = get_continuous_segments(time_range)
+    continuous_segments = get_continuous_segments(time_range, variation if variation else 2)
     display_text_lst = to_human_readable(continuous_segments)
     print(f"=== Activities in the last {math.floor((_end-_start).total_seconds()/3600)} hours ===")
     for text in display_text_lst:
